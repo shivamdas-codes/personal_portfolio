d5616d71ad9055ab69c2185ef39686b967963d73
shivam <shivam984861@gmail.com>
2026-01-15 20:53:07 +0530

    theme fixed

--- src/components/ui/themetoggle.tsx
+++ src/components/ui/themetoggle.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Moon, Sun } from "lucide-react"
-import { useTheme } from "@/hooks/useTheme"
+import { useTheme } from "@/hooks/theme"
 import { Button } from "./button"
 
 export function ThemeToggle() {
